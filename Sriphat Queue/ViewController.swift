ccda3766366a39fa82bc8ef714b5c8ffa0902677
Chaowarit Ongkum <chwrtokuma@gmail.com>
2018-01-05 11:30:45 +0700

    Initial Commit

---
+++ Sriphat Queue/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sriphat Queue
+//
+//  Created by Nattapol Chitinsiyanon on 5/1/2561 BE.
+//  Copyright © 2561 Sriphat Medical Center. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
